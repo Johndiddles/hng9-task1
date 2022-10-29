aeb101db187f6f3f319847aeb43da01e4b2f8b51
John <johnadepelumi@gmail.com>
2022-10-29 15:28:26 +0100

    added footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+import zuri from "../../assets/zuri.png";
+import i4g from "../../assets/I4G.png";
+
+const Footer = () => {
+  return (
+    <div className={styles.footer}>
+      <img className={styles.footer__img} src={zuri} alt="zuri" />
+      <div className={styles.footer__text}>HNG Internship 9 Frontend Task</div>
+      <img className={styles.footer__img + styles.i4g} src={i4g} alt="i4g" />
+    </div>
+  );
+};
+
+export default Footer;
