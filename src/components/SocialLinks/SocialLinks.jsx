8810c7ee36785d724fc402bea976fbf61add6151
John <johnadepelumi@gmail.com>
2022-10-29 15:30:12 +0100

    added social links

---
+++ src/components/SocialLinks/SocialLinks.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./SocialLinks.module.css";
+import slack from "../../assets/slack.png";
+import github from "../../assets/github.png";
+
+const SocialLinks = () => {
+  return (
+    <div className={styles.socialLinks__wrapper}>
+      <a href="https://slack.com">
+        <img src={slack} alt="slack" />
+      </a>
+      <a href="https://github.com/Johndiddles">
+        <img src={github} alt="slack" />
+      </a>
+    </div>
+  );
+};
+
+export default SocialLinks;
