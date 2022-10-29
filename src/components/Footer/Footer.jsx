b7f048f35a139601122774c9d368ef7cbf10bf38
John <johnadepelumi@gmail.com>
2022-10-29 15:47:04 +0100

    corrected image size in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,9 +7,17 @@ import i4g from "../../assets/I4G.png";
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      <img className={styles.footer__img} src={zuri} alt="zuri" />
+      <img
+        className={`${styles.footer__img} ${styles.zuri}`}
+        src={zuri}
+        alt="zuri"
+      />
       <div className={styles.footer__text}>HNG Internship 9 Frontend Task</div>
-      <img className={styles.footer__img + styles.i4g} src={i4g} alt="i4g" />
+      <img
+        className={`${styles.footer__img} ${styles.i4g}`}
+        src={i4g}
+        alt="i4g"
+      />
     </div>
   );
 };
