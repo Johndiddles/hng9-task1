9d62926467bc6face1aa7fb0f3d101a2424cd24d
John Adepelumi <johnadepelumi@Johns-MacBook-Air.local>
2022-11-05 13:05:18 +0100

    style: add custom box shadows to themes in tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        normal: "0px 1px 2px rgba(16, 24, 40, 0.05)",
+        focus: "00px 1px 2px rgba(16, 24, 40, 0.05), 0px 0px 0px 4px #D1E9FF",
+        required: "0px 1px 2px rgba(16, 24, 40, 0.05)",
+        error: "0px 1px 2px rgba(16, 24, 40, 0.05), 0px 0px 0px 4px #FFD3CC",
+      },
+    },
   },
   plugins: [],
 };
